0301a65333b995ed78a699915648f7279730934b
diegoalcantaradev <diegoalcantara2003@gmail.com>
2024-10-30 12:14:13 -0300

    upload img

--- src/controllers/Products/CreateController.js
+++ src/controllers/Products/CreateController.js
@@ -1,9 +1,33 @@
+const path = require("path");
 const ProductModel = require("../../models/ProductModel");
-
+const fs = require('fs');
 
 module.exports = async (request, response) => {
-    let {body} = request;
-    let product = await ProductModel.create(body);
+    let {
+        name,
+        slug,
+        price
+    } = request.body;
+        let product = await ProductModel.create({
+            name,slug,price
+        });
+    let {images} = request.body
+
+    let res = await fetch(images);
+    let type = res.headers.get('content-type');
+    let extesion = type.split('/').pop();
+    let buffer = Buffer.from(await res.arrayBuffer());
+    let filename = Math.random().toString(16).slice(2);
+    let directory = path.resolve(`public/${slug}`)
+
+    if(!fs.existsSync(directory)){
+        fs.mkdirSync(directory, {recursive: true});
+    }
+
+    let file = `${directory}/${filename}.${extesion}`
+
+    fs.writeFileSync(file, buffer)
+
     response.status(201);
     return response.json(product);
 }
